48b7864d9df1498a112a29f7ea518caf47d1bf2f
RJSakariya <amitdervadiya@gmail.com>
2024-05-01 16:05:30 +0530

    day 1

---
+++ Day 1/day 1.js
@@ -0,0 +1,23 @@
+// Question 1
+var age = "My age is 90"
+console.log(age)
+
+// Question 2
+var message = "Hello World!" 
+console.log(message)
+
+// Question 3
+var boolean = true
+console.log(typeof boolean)
+
+// Question 4
+var a = 5,b = 10,sum = a + b
+console.log(a + ' + ' + b + ' = ' + sum)
+
+// Question 5
+var length = 5, width = 3
+console.log("The area of the rectangle is: " + length*width)
+
+// Question 6
+var a = 5,b = 10,multiply = a + b
+console.log(a + ' * ' + b + ' = ' + multiply)
